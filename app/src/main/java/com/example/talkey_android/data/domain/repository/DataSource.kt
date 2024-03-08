120169e138088270fd08df3c4a37bd305edb49ba
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-03-08 13:49:25 +0100

    added BaseResponse.kt

--- app/src/main/java/com/example/talkey_android/data/domain/repository/DataSource.kt
+++ app/src/main/java/com/example/talkey_android/data/domain/repository/DataSource.kt
@@ -2,6 +2,7 @@ package com.example.talkey_android.data.domain.repository
 
 import com.example.talkey_android.data.domain.model.users.RegisterFromResponseModel
 import com.example.talkey_android.data.domain.model.users.RegisterToRequestModel
+import com.example.talkey_android.data.domain.repository.remote.response.BaseResponse
 
 interface DataSource {
     fun postRegister(registerToRequestModel: RegisterToRequestModel): BaseResponse<RegisterFromResponseModel>
