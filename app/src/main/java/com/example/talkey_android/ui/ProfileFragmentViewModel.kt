6301d6892c17de99e4f0927b1c02a45d784c1168
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-03-11 12:33:19 +0100

    added setOnlineUseCase

--- app/src/main/java/com/example/talkey_android/ui/ProfileFragmentViewModel.kt
+++ app/src/main/java/com/example/talkey_android/ui/ProfileFragmentViewModel.kt
@@ -2,10 +2,12 @@ package com.example.talkey_android.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.talkey_android.data.domain.model.common.MessageModel
 import com.example.talkey_android.data.domain.model.error.ErrorModel
 import com.example.talkey_android.data.domain.model.users.UserProfileModel
 import com.example.talkey_android.data.domain.repository.remote.response.BaseResponse
 import com.example.talkey_android.data.domain.use_cases.users.GetProfileUseCase
+import com.example.talkey_android.data.domain.use_cases.users.SetOnlineUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,14 +16,36 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class ProfileFragmentViewModel(
-    private val getProfileUseCase: GetProfileUseCase
+    private val getProfileUseCase: GetProfileUseCase,
+    private val setOnlineUseCase: SetOnlineUseCase
 ) : ViewModel() {
 
     private val _getProfileError = MutableSharedFlow<ErrorModel>()
     val getProfileError: SharedFlow<ErrorModel> = _getProfileError
-
     private val _userProfile = MutableStateFlow(UserProfileModel())
     val userProfile: StateFlow<UserProfileModel> = _userProfile
+
+    private val _setOnlineError = MutableSharedFlow<ErrorModel>()
+    val setOnlineError: SharedFlow<ErrorModel> = _setOnlineError
+    private val _setOnlineMessage = MutableStateFlow(MessageModel())
+    val setOnlineMessage: StateFlow<MessageModel> = _setOnlineMessage
+
+    fun setOnline(isOnline: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val baseResponse = setOnlineUseCase(
+                _userProfile.value.token, isOnline
+            )
+            when (baseResponse) {
+                is BaseResponse.Success -> {
+                    _setOnlineMessage.emit(baseResponse.data)
+                }
+
+                is BaseResponse.Error -> {
+                    _setOnlineError.emit(baseResponse.error)
+                }
+            }
+        }
+    }
 
     fun getProfile(token: String) {
         viewModelScope.launch(Dispatchers.IO) {
