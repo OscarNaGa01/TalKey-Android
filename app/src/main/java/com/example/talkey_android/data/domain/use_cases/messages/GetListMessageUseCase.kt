1e1e6aca304a13068f55940928629a2cad82d8a2
VASS\oscar.navarro <oscar.navarro@vasscompany.com>
2024-03-15 17:00:45 +0100

    get message list and send message use case ok

--- app/src/main/java/com/example/talkey_android/data/domain/use_cases/messages/GetListMessageUseCase.kt
+++ app/src/main/java/com/example/talkey_android/data/domain/use_cases/messages/GetListMessageUseCase.kt
@@ -1,16 +1,16 @@
 package com.example.talkey_android.data.domain.use_cases.messages
 
-import com.example.talkey_android.data.domain.model.common.SuccessModel
+import com.example.talkey_android.data.domain.model.messages.ListMessageModel
 import com.example.talkey_android.data.domain.repository.DataProvider
 import com.example.talkey_android.data.domain.repository.remote.response.BaseResponse
 
-class SendMessageUseCase {
+class GetListMessageUseCase {
     suspend operator fun invoke(
         token: String,
-        chat: String,
-        source: String,
-        message: String
-    ): BaseResponse<SuccessModel> {
-        return DataProvider.sendMessage(token, chat, source, message)
+        idChat: Int,
+        limit: Int,
+        offset: Int
+    ): BaseResponse<ListMessageModel> {
+        return DataProvider.getMessages(token, idChat, limit, offset)
     }
 }
