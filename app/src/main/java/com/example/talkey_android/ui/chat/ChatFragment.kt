f46ddfd9563b81e0cb139c48c5c2264c5da94263
VASS\oscar.navarro <oscar.navarro@vasscompany.com>
2024-03-22 14:54:20 +0100

    showing online status and contact profile image

--- app/src/main/java/com/example/talkey_android/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/talkey_android/ui/chat/ChatFragment.kt
@@ -10,22 +10,27 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.talkey_android.MainActivity
+import com.example.talkey_android.R
+import com.example.talkey_android.data.domain.use_cases.chats.GetListChatsUseCase
 import com.example.talkey_android.data.domain.use_cases.messages.GetListMessageUseCase
 import com.example.talkey_android.data.domain.use_cases.messages.SendMessageUseCase
 import com.example.talkey_android.data.utils.Utils
 import com.example.talkey_android.databinding.FragmentChatBinding
+import com.example.talkey_android.databinding.ItemProfileImageStatusChatBinding
 import com.example.talkey_android.ui.chat.adapter.ChatAdapter
 import kotlinx.coroutines.launch
 
 class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
+    private lateinit var profileImageStatusBinding: ItemProfileImageStatusChatBinding
     private lateinit var chatAdapter: ChatAdapter
     private lateinit var linearLayout: LinearLayoutManager
     private val args: ChatFragmentArgs by navArgs()
     private val chatFragmentViewModel: ChatFragmentViewModel =
-        ChatFragmentViewModel(SendMessageUseCase(), GetListMessageUseCase())
+        ChatFragmentViewModel(SendMessageUseCase(), GetListMessageUseCase(), GetListChatsUseCase())
     private lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
@@ -33,6 +38,7 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentChatBinding.inflate(inflater, container, false)
+        profileImageStatusBinding = ItemProfileImageStatusChatBinding.bind(binding.root)
         mainActivity = requireActivity() as MainActivity
         requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         return binding.root
@@ -43,7 +49,6 @@ class ChatFragment : Fragment() {
         observeViewModel()
         initListeners()
         getMessageList()
-        binding.tvName.text = args.nickTarget
     }
 
     private fun initListeners() {
@@ -80,10 +85,36 @@ class ChatFragment : Fragment() {
     }
 
     private fun getMessageList() {
-        chatFragmentViewModel.getMessages(args.token, args.idChat, 200, 0)
+        chatFragmentViewModel.getMessages(args.token, args.idChat, 20, 0)
+        chatFragmentViewModel.getContactData(args.token, args.idChat)
     }
 
     private fun observeViewModel() {
+        lifecycleScope.launch {
+            chatFragmentViewModel.contact.collect { userData ->
+                binding.tvName.text = userData.targetNick
+
+                Glide.with(requireContext())
+                    .load(userData.targetAvatar)
+                    .error(R.drawable.image)
+                    .into(profileImageStatusBinding.ivProfile)
+                println(userData.targetOnline)
+
+                if (userData.targetOnline) {
+                    Glide.with(requireContext())
+                        .load(R.color.statusOnline)
+                        .error(R.color.statusOnline)
+                        .into(profileImageStatusBinding.ivStatus)
+
+                } else {
+                    Glide.with(requireContext())
+                        .load(R.color.statusOffline)
+                        .error(R.color.statusOffline)
+                        .into(profileImageStatusBinding.ivStatus)
+                }
+            }
+        }
+
         lifecycleScope.launch {
             chatFragmentViewModel.message.collect { messages ->
                 Utils.showDateOnce(messages)
