4c26e03b8dc1d90e363e9b632d8d67cc3db0e088
VASS\oscar.navarro <oscar.navarro@vasscompany.com>
2024-03-27 10:28:50 +0100

    removed useless paging implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,10 +89,6 @@ dependencies {
     //Swiperefreshlayout
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
-    //Paging
-    implementation("androidx.paging:paging-common-ktx:3.2.1")
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
