4360f748fbaa1919c48a41243adb6494e305e337
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-04-02 12:49:34 +0200

    working at notifications

---
+++ app/src/main/java/com/example/talkey_android/data/utils/MyFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.example.talkey_android.data.utils
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+}
